70d40856ac902ef1c1231a90cadac9ff8b4f2153
lukajaa <lucaskchang@gmail.com>
2023-06-08 12:46:42 -0700

    gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   target: 'static',
-    modules: [
-        '@nuxtjs/tailwindcss'
-      ]
+  modules: [
+      '@nuxtjs/tailwindcss'
+  ],
+  gtag: {
+    id: 'G-QNLZ5NY7HH'
+  }  
 })
